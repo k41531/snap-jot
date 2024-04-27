f3c01e84aa97c25a56c319ee6dbe199e4c76c956
k41531 <me@kaisei.dev>
2024-04-27 18:53:17 +0900

    Pull contributions

--- src/memo.tsx
+++ src/memo.tsx
@@ -1,71 +1,91 @@
-import { Form, ActionPanel, Action, getPreferenceValues, openExtensionPreferences } from "@raycast/api";
+import {
+  Form,
+  ActionPanel,
+  Action,
+  getPreferenceValues,
+  openExtensionPreferences,
+  LaunchProps,
+  popToRoot,
+  Toast,
+  showToast,
+} from "@raycast/api";
 import { useForm } from "@raycast/utils";
+import fs from "fs";
+import path from "path";
+import { formatDateTime, replaceDatePlaceholders } from "./utils/FormatDateTime";
 
 interface Memo {
-    memo: string;
+  memo: string;
 }
 
 interface Preferences {
-    directory: string;
-    format: string;
-    prefix: string;
-    template: string;
+  directory: string;
+  format: string;
+  prefix: string;
+  timeFormat: string;
+  template: string;
 }
 
-function formatDateTime(date: Date, format: string) {
-    const tokens: { [key: string]: string } = { // Add index signature to tokens object
-        YYYY: date.getFullYear().toString(),
-        YY: date.getFullYear().toString().slice(-2),
-        MM: (date.getMonth() + 1).toString().padStart(2, '0'),
-        DD: date.getDate().toString().padStart(2, '0'),
-        HH: date.getHours().toString().padStart(2, '0'),
-        mm: date.getMinutes().toString().padStart(2, '0'),
-        ss: date.getSeconds().toString().padStart(2, '0')
-    };
+export default function Command(props: LaunchProps<{ draftValues: Memo }>) {
+  const preferences = getPreferenceValues<Preferences>();
+  const { directory, format, timeFormat, prefix, template } = preferences;
+  const { draftValues } = props;
 
-    return format.replace(/YYYY|YY|MM|DD|HH|mm|ss/g, match => tokens[match]);
-}
+  // if prefix is not include "A" and timeFormat is 12, add "A" to prefix.
+  const timestamp = timeFormat === "12" && !prefix.includes("A") ? prefix + "A " : prefix;
 
-function replaceDatePlaceholders(date: Date, text: string): string {
-    return text.replace(/{{date:([^}]+)}}/g, (_, dateFormat) => formatDateTime(date, dateFormat));
-}
-
-
-export default function Command() {
-    const preferences = getPreferenceValues<Preferences>();
-    const { directory, format, prefix, template } = preferences;
-
-    function saveMemo(values: Memo) {
-        const fs = require('fs');
-        const path = require('path');
-        const filePath = path.join(directory, formatDateTime(new Date(), format));
-        let memo = formatDateTime(new Date(), prefix) + values.memo + "\n";
-        // ファイルが存在しない場合は、ファイルの先頭にテンプレートを追加する
-        if (!fs.existsSync(filePath)) {
-            // テンプレートのファイルパスのファイルないの文字列を取得する
-            const templateContent = fs.readFileSync(template, 'utf8');
-            memo = replaceDatePlaceholders(new Date(), templateContent) + "\n" + memo;
-        }
-        fs.appendFileSync(filePath, memo);
-
+  function saveMemo(values: Memo) {
+    try {
+      const filePath = path.join(directory, formatDateTime(new Date(), format));
+      const memo = formatDateTime(new Date(), timestamp, timeFormat === "12") + values.memo + "\n";
+      let memoContent = memo;
+      // if file is not exist, create file and write memo.
+      if (!fs.existsSync(filePath)) {
+        // if template is empyt, content is empty.
+        const templateContent = !template ? "" : fs.readFileSync(template, "utf8");
+        memoContent = replaceDatePlaceholders(new Date(), templateContent) + "\n" + memo;
+      }
+      fs.appendFileSync(filePath, memoContent);
+      const successOptions: Toast.Options = {
+        style: Toast.Style.Success,
+        title: "Memo Saved",
+        message: memo,
+      };
+      showToast(successOptions);
+    } catch (error) {
+      const failureOptions: Toast.Options = {
+        style: Toast.Style.Failure,
+        title: "Error Saving Memo",
+        message: (error as Error).message,
+      };
+      showToast(failureOptions);
     }
-    const { handleSubmit, reset, itemProps } = useForm<Memo>({
-        onSubmit(values) {
-            saveMemo(values);
-            reset();
-        }
-    });
+  }
 
-    return (
-        <Form
-            enableDrafts={true}
-            actions={
-                <ActionPanel>
-                    <Action.SubmitForm title="Submit" onSubmit={handleSubmit} />
-                    <Action title="Open Extension Preferences" onAction={openExtensionPreferences} />
-                </ActionPanel>
-            }>
-            <Form.TextArea title="Memo" {...itemProps.memo} />
-        </Form>
-    )
+  const { handleSubmit, reset, itemProps } = useForm<Memo>({
+    onSubmit(values) {
+      saveMemo(values);
+      reset();
+      popToRoot();
+    },
+  });
+
+  return (
+    <Form
+      enableDrafts={true}
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Submit" onSubmit={handleSubmit} />
+          <Action title="Open Extension Preferences" onAction={openExtensionPreferences} />
+        </ActionPanel>
+      }
+    >
+      <Form.TextArea
+        title="Memo"
+        {...itemProps.memo}
+        placeholder="Whats on your mind..."
+        defaultValue={draftValues?.memo}
+      />
+    </Form>
+  );
 }
